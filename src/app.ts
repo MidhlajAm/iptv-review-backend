20e2601b53fa84071f7464ccfcbe2aa820195153
Midhlaj Am <midhlaj.am786@gmail.com>
2026-06-18 19:36:59 +0530

    `Updated live streams, series info, and VOD streams data with new URLs`

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import xtreamRoutes from './routes/xtream.routes';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.POupdaye thumbnailRT || 3000;
 
 // CORS enabled for all origins (demo environment)
 app.use(cors());
